feb65f93f4381031d20158df6c8b1f29915cd3d7
DerBaumann <natur5210@gmail.com>
2025-12-14 21:01:28 +0100

    Make example View to integrate lombok and jpa.

---
+++ src/main/java/com/example/application/repositories/PersonRepo.java
@@ -0,0 +1,9 @@
+package com.example.application.repositories;
+
+import com.example.application.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepo extends JpaRepository<Person, Long> {
+}
