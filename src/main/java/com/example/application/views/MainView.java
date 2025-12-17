c758e3c6757f2c41d7c4e23947e0b15be00a8b7d
DerBaumann <natur5210@gmail.com>
2025-12-17 15:18:52 +0100

    Add basic calorie progress bar

--- src/main/java/com/example/application/views/MainView.java
+++ src/main/java/com/example/application/views/MainView.java
@@ -3,27 +3,34 @@ package com.example.application.views;
 import com.example.application.entities.Meal;
 import com.example.application.services.GreetService;
 import com.example.application.services.MealService;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 
 @Route
 public class MainView extends VerticalLayout {
-
     public MainView(MealService mealService) {
-        var h1 = new H1("Hello World");
-        var p = new Paragraph("This is my Vaadin App");
+        final var meals = mealService.findAll();
+        final var totalCalories = meals.stream().mapToInt(Meal::getCalories).sum();
+        final var maxCalories = 2000;
 
-        add(h1, p);
+        final var calorieBar = new ProgressBar();
+        calorieBar.setValue((double) totalCalories /  maxCalories);
+        calorieBar.setHeight(4, Unit.EM);
 
-        var meals = mealService.findAll();
+        final var calorieStatusText = String.format("%d / %d", totalCalories, maxCalories);
+        final var calorieStatus = new Paragraph(calorieStatusText);
+        add(calorieBar, calorieStatus);
+
         if (meals.isEmpty()) {
             add(new Paragraph("No meals found"));
         } else {
             var mealList = new UnorderedList();
-            for  (Meal meal : mealService.findAll()) {
+            for (Meal meal : mealService.findAll()) {
                 var listItem = new ListItem(meal.getName());
                 mealList.add(listItem);
             }
